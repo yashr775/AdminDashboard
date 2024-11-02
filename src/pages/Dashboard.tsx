950dd94437137396deedcdcffc0ad4f3ffd73d71
yashr775 <rajy29629@gmail.com>
2024-11-02 18:18:14 +0530

    some mixin created

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import AdminSidebar from "../components/AdminSidebar";
+
+const Dashboard = () => {
+  return (
+    <div className="adminContainer">
+      <AdminSidebar />
+      <main></main>
+    </div>
+  );
+};
+
+export default Dashboard;
