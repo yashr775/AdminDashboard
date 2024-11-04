68065b29b85c673301010e0e8f1e4e3802b6866e
yashr775 <rajy29629@gmail.com>
2024-11-04 22:05:52 +0530

    EOD commit

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,12 +1,47 @@
+import { IconType } from "react-icons";
+import { RiDashboardFill, RiShoppingBag3Line } from "react-icons/ri";
+import { Link, useLocation } from "react-router-dom";
+
 const AdminSidebar = () => {
+  const location = useLocation();
   return (
     <aside>
       <h2>LOGO .</h2>
       <div>
         <h5>Dashboard</h5>
+        <ul>
+          <Li
+            url="/admin/dashboard"
+            text="Dashboard"
+            Icon={RiDashboardFill}
+            location={location}
+          ></Li>
+          <Li
+            url="/admin/product"
+            text="Products"
+            Icon={RiShoppingBag3Line}
+            location={location}
+          ></Li>
+        </ul>
       </div>
     </aside>
   );
 };
+
+interface LiProps {
+  url: string;
+  text: string;
+  location: Location;
+  Icon: IconType;
+}
+
+const Li = ({ url, text, location, Icon }: LiProps) => (
+  <li>
+    <Link to={url}>
+      <Icon />
+      {text}
+    </Link>
+  </li>
+);
 
 export default AdminSidebar;
