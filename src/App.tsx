b02c7eed77addf9b8595be3f6d8b486c6dc73225
yashr775 <rajy29629@gmail.com>
2024-11-15 16:25:49 +0530

    Products page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import Loading from "./components/Loading";
+import Loading from "./components/Loader";
 
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Products = lazy(() => import("./pages/Products"));
